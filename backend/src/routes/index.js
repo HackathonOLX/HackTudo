1f26ec7eb1c3dfedcbf3d05a3b354693b44fc37f
akihito7 <akihitopro7@gmail.com>
2023-10-19 13:43:42 -0300

    tentando...

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,9 @@ const advertsRoutes = require("./adverts.routes")
 const routes = Router();
 
 routes.use("/adverts", advertsRoutes);
+routes.get("/", (request, response) => {
+    response.status(200).json("hello world")
+})
 
 module.exports = routes;
 
